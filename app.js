4bb0a78b9fd7389a97d37932a1527d72bc9fdb00
Abel Franco <abelmatiasf@gmail.com>
2022-03-27 21:51:15 -0400

    [1.3.9] Acces-control cors

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 /**
  * Swagger route
  */
